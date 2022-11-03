07638fd3c864122a7a591feff7f26d0f834c5ad6
Martin Pochat-Cottilloux <72661166+martinpctx@users.noreply.github.com>
2022-11-03 17:44:17 +0200

    Add info about last location sent

---
+++ Mobile snowledge/mobile_app/lib/helper/utility.dart
@@ -0,0 +1,35 @@
+import 'package:intl/intl.dart';
+
+class Utility {
+  /// Returns the time between the date passed in arguments and the current date
+  static String getTimeAgo(String? date) {
+    if (date == null || date.isEmpty) {
+      return '';
+    }
+    String time = '';
+    var dt = DateTime.parse(date).toLocal();
+
+    if (DateTime.now().toLocal().isBefore(dt)) {
+      return DateFormat.jm().format(DateTime.parse(date).toLocal()).toString();
+    }
+
+    var dur = DateTime.now().toLocal().difference(dt);
+    if (dur.inDays > 365) {
+      time = DateFormat.yMMMd().format(dt);
+    } else if (dur.inDays > 30) {
+      time = DateFormat.yMMMd().format(dt);
+    } else if (dur.inDays > 0) {
+      time = '${dur.inDays} d';
+      return dur.inDays == 1 ? '1 d' : DateFormat.MMMd().format(dt);
+    } else if (dur.inHours > 0) {
+      time = '${dur.inHours} h';
+    } else if (dur.inMinutes > 0) {
+      time = '${dur.inMinutes} m';
+    } else if (dur.inSeconds > 0) {
+      time = '${dur.inSeconds} s';
+    } else {
+      time = 'now';
+    }
+    return time;
+  }
+}
