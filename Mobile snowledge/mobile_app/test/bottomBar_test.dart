18aa392fd4a29f0fe2468aae2585f3bdb7ad544b
Martin Pochat-Cottilloux <72661166+martinpctx@users.noreply.github.com>
2022-12-16 11:38:25 +0100

    Fixing bug

--- Mobile snowledge/mobile_app/test/bottomBar_test.dart
+++ Mobile snowledge/mobile_app/test/bottomBar_test.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_app/bottom_bar/bottomBar.dart';
+import 'package:mobile_app/state/appState.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets(
-      'Show Rescue dialog onTap',
-          (tester) async {
-        await tester.pumpWidget(const MaterialApp(
+  testWidgets('Show Rescue dialog onTap', (tester) async {
+    await tester.pumpWidget(MultiProvider(
+        providers: [
+          ChangeNotifierProvider<AppState>(create: (_) => AppState())
+        ],
+        child: const MaterialApp(
           home: Scaffold(
             bottomNavigationBar: BottomBar(),
           ),
-        ));
-        await tester.tap(find.text('Pyydäapua'));
-        await tester.pumpAndSettle();
+        )));
+    await tester.tap(find.text('Pyydä\napua'));
+    await tester.pumpAndSettle();
 
-        expect(find.text('Millaista apua tarvitset?'), findsOneWidget);
-      });
+    expect(find.text('Millaista apua tarvitset?'), findsOneWidget);
+  });
 
-  testWidgets(
-      'Show Sharing Location dialog onTap',
-          (tester) async {
-        await tester.pumpWidget(const MaterialApp(
-          home: Scaffold(
-            bottomNavigationBar: BottomBar(),
-          ),
-        ));
-        await tester.tap(find.byIcon(Icons.near_me_rounded));
-        await tester.pumpAndSettle();
+  testWidgets('Show Sharing Location dialog onTap', (tester) async {
+    await tester.pumpWidget(MultiProvider(
+        providers: [
+          ChangeNotifierProvider<AppState>(create: (_) => AppState())
+        ],
+        child: const MaterialApp(
+      home: Scaffold(
+        bottomNavigationBar: BottomBar(),
+      ),
+    )));
+    await tester.tap(find.text('Sijainti'));
+    await tester.pumpAndSettle();
 
-        expect(find.text('Sijaintitiedon jakaminen'), findsOneWidget);
-      });
+    expect(find.text('Sijaintitiedon jakaminen'), findsOneWidget);
+  });
 }
