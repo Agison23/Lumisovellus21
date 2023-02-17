4a91d51db86ac1d2bafbcf4d5194219db75cd367
An Nguyen <nhatan020902@gmail.com>
2023-02-17 22:18:11 +0700

    update credential for local testing

--- src/testSql/Kayttajat.sql
+++ src/testSql/Kayttajat.sql
@@ -1,5 +1,5 @@
 INSERT INTO Kayttajat(Etunimi, Sukunimi, Sähköposti, Salasana, Rooli)
-VALUES ('admin', 'admin', 'admin@gmail.com','admin', 'admin')
-       ('operatir', 'operator', 'operator@gmail.com','operator','operator')
-       ('dummy','1','dummy1@gmail.com','dummy1','operator'),
-       ('dummy','2','dummy2@gmail.com','dummy2','admin')
+VALUES ('admin', 'admin', 'admin@gmail.com','$2y$15$ydbMWEgUhFrH5EjewyPpiu1kCgBAa9NUR3q7ejPO3tbrxOpZ0sgUu', 'admin'),
+       ('operator', 'operator', 'blank@gmail.com','$2y$15$dx.8V4uPNQ1rEzAe2pDF7uqos6RDYLp0X.rQJiHT7s1SGlKprzeLu','operator'),
+       ('dummy','1','dummy1@gmail.com','$2y$15$yTf9gDO9HrP7aXCSXvuf7u5rofn4p0ZsRihUmhOZdVy1HmPKGssr.','operator'),
+       ('dummy','2','dummy2@gmail.com','$2y$15$8UjH5s7MuTO4Z9s8Q7l/ee05FoQbRA1V3b3hbiLeh2AB0ovizbumu','admin');
--- src/testSql/Paivitykset.sql
+++ src/testSql/Paivitykset.sql
@@ -1,3 +1,3 @@
-INSERT INTO Paivitykset(Segmentti, Lumilaatu, Lisätiedot, Kommentti)
-VALUES ("1",1,1,"comment")
-       ("2",1,2,"comment 2")
+INSERT INTO paivitykset(Tekija,Segmentti, Aika, Kuvaus,Lumilaatu_ID1,Lumilaatu_ID2,Toissijainen_ID1,Toissijainen_ID2)
+VALUES (1,5,NOW(),"comment",1,2,1,2),
+       (1,6,NOW(),"comment",1,2,1,2);
