d0a1d1fca3ae10934bc21db7b3dfba603776bf67
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2023-02-23 10:56:18 +0000

    Fixes to the cypress config file and add a unit test directory

---
+++ src/map-app/cypress/unitTests/example.cy.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BottomNav from '../../src/components/BottomNav'
+
+// This is an example file with no actual tests
+describe('<BottomNav />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<BottomNav />)
+  })
+})
