e1a525529705d8ae8b6a206f294b991fd41fdfa7
otso tikkanen <otso.tikkanen@tuni.fi>
2023-03-16 10:51:56 +0200

    #217: add tests for language fetures and fix old tests in web

--- src/map-app/cypress/integrationTests/loginTests.cy.js
+++ src/map-app/cypress/integrationTests/loginTests.cy.js
@@ -1,12 +1,14 @@
+///<reference types="cypress"/>
+
 describe("Login", () => {
   //Desktop
-  it("Administrator login and get access to user managing tab", () => {
-    const email = "test@test.fi";
-    const password = "testi";
+  it("Administrator login and get access to user managing tab", ()=>{
+    const email = "admin@gmail.com";
+    const password = "admin";
 
     cy.visit("/");
-    cy.findByRole("button", { name: /openloginwindow/i }).click();
-    cy.findByRole("textbox", { name: /email/i }).type(email);
+    cy.findByRole("button", {  name: /openloginwindow/i}).click();
+    cy.findByRole("textbox", {  name: /sähköposti/i}).type(email);
     cy.findByLabelText(/salasana/i).type(password);
     cy.findByRole("button", { name: /kirjaudu/i }).click();
     cy.findByRole("button", { name: /hallitse/i }, { timeout: 15000 }).click();
@@ -14,19 +16,17 @@ describe("Login", () => {
     cy.findByText(/lisää käyttäjä/i).should("be.visible");
   });
 
-  it("Operator login and doesn't have admin rights", () => {
-    const email = "operator@operator.fi";
+  it("Operator login and doesn't have admin rights", ()=> {
+    const email = "blank@gmail.com";
     const password = "operator";
 
     cy.visit("/");
-    cy.findByRole("button", { name: /openloginwindow/i }).click();
-    cy.findByRole("textbox", { name: /email/i }).type(email);
+    cy.findByRole("button", {  name: /openloginwindow/i}).click();
+    cy.findByRole("textbox", {  name: /sähköposti/i}).type(email);
     cy.findByLabelText(/salasana/i).type(password);
     cy.findByRole("button", { name: /kirjaudu/i }).click();
-    cy.findByRole("button", { name: /hallitse/i }, { timeout: 15000 }).click();
-    cy.findByRole("button", { name: /käyttäjät/i }).click();
-    cy.findByRole("heading", {
-      name: /käyttäjähallinta vaatii admin-oikeudet/i,
-    });
+    cy.findByRole("button", { name: /hallitse/i },{timeout: 15000}).click();
+    cy.findByRole("button", {  name: /käyttäjät/i}).click();
+    cy.findByText( /Käyttäjähallinta vaatii admin-oikeude/i).should("be.visible");
   });
 });
---
+++ src/map-app/cypress/unitTests/loginLanButtonTest.cy.jsx
@@ -0,0 +1,19 @@
+import Selector from '../../src/components/select';
+
+
+describe("Login", () => {
+
+  //Desktop
+  it("user presses change language button before logging in", ()=>{
+
+
+
+      cy.mount(<Selector />)
+
+      //cy.get('[data-cy=increment]').click()
+      //cy.get('@onChangeSpy').should('have.been.calledWith', 1)
+  })
+
+    
+});
+
