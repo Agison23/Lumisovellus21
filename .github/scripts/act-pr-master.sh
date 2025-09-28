6582b208d98c6cc35b8362f04080f6ac02b5d215
eznt <eznt@users.noreply.github.com>
2025-09-28 15:55:15 +0300

    Remove reuse flag from act scripts

--- .github/scripts/act-pr-master.sh
+++ .github/scripts/act-pr-master.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 act pull_request \
   -W .github/workflows/pr-to-master.yml \
   -e .github/events/pr-to-master.json \
   -j rc \
   -P ubuntu-22.04=ghcr.io/catthehacker/ubuntu:act-22.04 \
-  --pull=false \
-  --reuse
+  --pull=false
--- .github/scripts/act-pr-release.sh
+++ .github/scripts/act-pr-release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 act pull_request \
   -W .github/workflows/pr-to-release.yml \
   -e .github/events/pr-to-release.json \
   -j integration \
   -P ubuntu-22.04=ghcr.io/catthehacker/ubuntu:act-22.04 \
-  --pull=false \
-  --reuse
+  --pull=false
