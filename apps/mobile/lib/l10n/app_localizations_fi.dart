3b5fbbb92b5671e0844326c50b47babbd55b7460
Huyen Pham <huyen.pham@tuni.fi>
2025-10-09 19:55:53 +0300

    Upgrade flutter & dependencies

--- apps/mobile/lib/l10n/app_localizations_fi.dart
+++ apps/mobile/lib/l10n/app_localizations_fi.dart
@@ -1,11 +1,16 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
 import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
 
 /// The translations for Finnish (`fi`).
 class AppLocalizationsFi extends AppLocalizations {
   AppLocalizationsFi([String locale = 'fi']) : super(locale);
 
   @override
-  String get snowAppInfo => 'Pallaksen Pöllöjen tuottama lumisovellus tarjoaa tietoja alueella vallitsevista lumiolosuhteista.';
+  String get snowAppInfo =>
+      'Pallaksen Pöllöjen tuottama lumisovellus tarjoaa tietoja alueella vallitsevista lumiolosuhteista.';
 
   @override
   String get next => 'Seuraava';
@@ -14,7 +19,8 @@ class AppLocalizationsFi extends AppLocalizations {
   String get sharingLocation => 'SIJAINTITIEDON JAKAMINEN';
 
   @override
-  String get rescueFeatureInfo => 'Sovelluksen pelastustoiminto kerää tietoja sijainnistasi. Sen avulla tarjoamme pelastamiseen tukea. Sijaintia käyttäen pelastuslaitos voi hyödyntää reittiäsi ja voit pyytää apua ympärillä olevilta kulkijoilta. Myös sinä voit auttaa muita. Voit koska tahansa poistaa sijainnin käytöstä.';
+  String get rescueFeatureInfo =>
+      'Sovelluksen pelastustoiminto kerää tietoja sijainnistasi. Sen avulla tarjoamme pelastamiseen tukea. Sijaintia käyttäen pelastuslaitos voi hyödyntää reittiäsi ja voit pyytää apua ympärillä olevilta kulkijoilta. Myös sinä voit auttaa muita. Voit koska tahansa poistaa sijainnin käytöstä.';
 
   @override
   String get allowSharing => 'SALLI JAKO';
@@ -26,7 +32,8 @@ class AppLocalizationsFi extends AppLocalizations {
   String get correctInfo => 'Syötäthän oikeat tietosi';
 
   @override
-  String get infoUsage => 'Tietojasi käytetään sovelluksen pelastustoimintoon.\nToiminnon avulla pelastuslaitos voi hälytyksen tapahtuessa löytää sinut helpommin.';
+  String get infoUsage =>
+      'Tietojasi käytetään sovelluksen pelastustoimintoon.\nToiminnon avulla pelastuslaitos voi hälytyksen tapahtuessa löytää sinut helpommin.';
 
   @override
   String get fName => 'Etunimi';
