5499c8324c8fe27ecfbde2bd594afab28e50d594
eemeli ruohomaki <eemeliruoh@gmail.com>
2025-10-11 21:57:16 +0300

    init next-intl

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
+import { useTranslations } from "next-intl"
 import Link from 'next/link'
- 
+
 export default function Page() {
+  const t = useTranslations('MapPage')
   return (
     <div>
-      <h1>Home</h1>
+
+      <h1>{t('title')}</h1>
+      <p>{t('loading')}</p>
       <Link href="/about">About</Link>
     </div>
   )
