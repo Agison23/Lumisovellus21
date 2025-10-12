4c13a2dd2c961f9febdb273922969e377daa2a25
eemeli ruohomaki <eemeliruoh@gmail.com>
2025-10-12 21:23:24 +0300

    ui fixes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -38,14 +38,14 @@ export default async function RootLayout({
             <SidebarProvider defaultOpen={defaultOpen}>
               <div className="w-[100dvw] h-[100dvh] flex flex-row overflow-hidden">
             <AppSidebar />
-            <SidebarInset className="p-2 relative flex flex-col gap-2">
-              <div className="w-full flex justify-start items-center ">
+            <SidebarInset className="relative flex flex-col gap-2 flex-1">
+              <div className="w-full flex justify-start items-center absolute z-999 p-2">
                 <SidebarTrigger className="bg-background" />
               </div>
-              <div className="flex-1 w-full h-full">
+              <div className="flex-1 w-full h-full rounded-xl overflow-clip">
                 {children}
               </div>
-              <div className="w-full flex justify-center">
+              <div className="w-full flex justify-center absolute bottom-1 p-2 z-9999">
                 <Nav />
               </div>
             </SidebarInset>
