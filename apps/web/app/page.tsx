c12914b731f6aaaa70dce65a6c780bb1efb8b020
eemeli ruohomäki <eemeliruoh@gmail.com>
2025-10-15 23:43:41 +0300

    bring in old segment data from legacy codebase

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
 import Map3d from "@/components/map-3d"
-import { mapAreas } from "@/lib/map/mock-data"
+import { mapAreas, mapAreas2 } from "@/lib/map/mock-data"
 import { useTranslations } from "next-intl"
 
 export default function Page() {
   const t = useTranslations('MapPage')
   return (
     <div className="w-full h-full">
-      <Map3d areas={mapAreas} />
+      <Map3d areas={mapAreas2} />
     </div>
   )
 }
