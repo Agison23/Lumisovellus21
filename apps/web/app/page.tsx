19ed806e70be14374d64874ade6cc37d8a73c068
eemeli ruohomäki <eemeliruoh@gmail.com>
2025-10-16 00:46:02 +0300

    map background should always be white

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
 import { useTranslations } from "next-intl"
 import Map3d from "@/components/map-3d"
-import { mapAreas } from "@/lib/map/mock-data"
+import { mapAreas2 } from "@/lib/map/mock-data"
 
 export default function Page() {
   const t = useTranslations('MapPage')
   return (
-    <div className="w-full h-full">
-      <Map3d areas={mapAreas} />
+    <div className="w-full h-full bg-white">
+      <Map3d areas={mapAreas2} />
     </div>
   )
 }
