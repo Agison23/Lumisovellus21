536990b27ce4e80f1897d0ef81f5e9d1c0d46ef1
eemeli ruohomaki <eemeliruoh@gmail.com>
2025-11-17 22:43:17 +0200

    i guess

--- apps/web/lib/map/loaders.ts
+++ apps/web/lib/map/loaders.ts
@@ -1,4 +1,4 @@
-import { Monitor } from '../snower/types';
+import { Monitor } from "../snower/types";
 import {
   InteractiveAreaFeature,
   SnowType,
@@ -6,19 +6,20 @@ import {
   mapAreas2,
   mockSnowData,
   mockUpdateData,
-} from './mock-data';
-import type { paths } from '@lumisovellus/api-client-web';
+} from "./mock-data";
+import type { paths } from "@lumisovellus/api-client-web";
 
 export type SegmentsResponse =
-  paths['/api/v1/segments']['get']['responses']['200']['content']['application/json'];
-export type Segment = SegmentsResponse['data'][number];
-const apiUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
+  paths["/api/v1/segments"]["get"]["responses"]["200"]["content"]["application/json"];
+export type Segment = SegmentsResponse["data"][number];
+export const apiUrl =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 
 // for now, mock fetching from the API
-export const fetchAreas = async (): Promise<SegmentsResponse['data']> => {
+export const fetchAreas = async (): Promise<SegmentsResponse["data"]> => {
   const res = await fetch(`${apiUrl}/api/v1/segments`);
   if (!res.ok) {
-    throw new Error('Failed to fetch areas');
+    throw new Error("Failed to fetch areas");
   }
 
   const data: SegmentsResponse = await res.json();
@@ -26,12 +27,12 @@ export const fetchAreas = async (): Promise<SegmentsResponse['data']> => {
 };
 
 export type SnowTypesResponse =
-  paths['/api/v1/snow-types']['get']['responses']['200']['content']['application/json'];
+  paths["/api/v1/snow-types"]["get"]["responses"]["200"]["content"]["application/json"];
 
-export const fetchSnowTypes = async (): Promise<SnowTypesResponse['data']> => {
+export const fetchSnowTypes = async (): Promise<SnowTypesResponse["data"]> => {
   const res = await fetch(`${apiUrl}/api/v1/snow-types`);
   if (!res.ok) {
-    throw new Error('Failed to fetch snow types');
+    throw new Error("Failed to fetch snow types");
   }
 
   const data: SnowTypesResponse = await res.json();
@@ -39,12 +40,12 @@ export const fetchSnowTypes = async (): Promise<SnowTypesResponse['data']> => {
 };
 
 export type ReportsResponse =
-  paths['/api/v1/observations']['get']['responses']['200']['content']['application/json'];
+  paths["/api/v1/observations"]["get"]["responses"]["200"]["content"]["application/json"];
 
-export const fetchUpdateData = async (): Promise<ReportsResponse['data']> => {
+export const fetchUpdateData = async (): Promise<ReportsResponse["data"]> => {
   const res = await fetch(`${apiUrl}/api/v1/observations`);
   if (!res.ok) {
-    throw new Error('Failed to fetch update data');
+    throw new Error("Failed to fetch update data");
   }
 
   const data: ReportsResponse = await res.json();
@@ -52,11 +53,11 @@ export const fetchUpdateData = async (): Promise<ReportsResponse['data']> => {
 };
 
 export const fetchMonitorData = async (): Promise<Monitor[]> => {
-  const res = await fetch('/api/snower');
+  const res = await fetch("/api/snower");
   // throw new Error("Mock error fetching monitor data");
 
   if (!res.ok) {
-    throw new Error('Failed to fetch monitor data');
+    throw new Error("Failed to fetch monitor data");
   }
   return res.json();
 };
