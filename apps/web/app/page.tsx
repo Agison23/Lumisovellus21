d189540551910f0cec608cc9f18294de0be4d058
eemeli ruohomaki <eemeliruoh@gmail.com>
2025-12-01 21:00:43 +0200

    slight ui tweaks

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,11 @@
-import { useTranslations } from "next-intl"
-import Map3d from "@/components/map-3d"
+import { useTranslations } from "next-intl";
+import Map3d from "@/components/map-3d";
 
 export default function Page() {
-  const t = useTranslations('MapPage')
+  const t = useTranslations("MapPage");
   return (
     <div className="w-full h-full bg-white">
-      <h1>{t('title')}</h1>
       <Map3d />
     </div>
-  )
+  );
 }
